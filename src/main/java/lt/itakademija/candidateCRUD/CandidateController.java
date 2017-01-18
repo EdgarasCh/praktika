5b07a4baf848f00be992c7b1112f3a4ecc5e5fe2
LinasPetrenas <linas.petrenas@gmail.com>
2017-01-18 21:43:37 +0200

    Add files via upload

--- src/main/java/lt/itakademija/candidateCRUD/CandidateController.java
+++ src/main/java/lt/itakademija/candidateCRUD/CandidateController.java
@@ -1,12 +1,16 @@
 package lt.itakademija.candidateCRUD;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class CandidateController {
@@ -15,8 +19,8 @@ public class CandidateController {
 	private CandidateService service;
 
 	@PostMapping("/api/candidate")
-	public CandidateEntity createOrUpdateCandidate(@RequestBody CandidateEntity c) {
-		return service.save(c);
+	public void createOrUpdateCandidate(@RequestBody CandidateEntity c) {
+		service.save(c);
 	}
 
 	@GetMapping("/api/candidate")
@@ -27,5 +31,14 @@ public class CandidateController {
 	@DeleteMapping("/api/candidate/{id}")
 	public CandidateEntity delete(@PathVariable Long id) {
 		return service.delete(id);
+	}
+
+	@PostMapping("/api/candidate/uploud")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public void createOrUpdateCandidateFromCSV(@RequestParam("file") MultipartFile csvFile,
+			@RequestParam("party-name") String partyName) {
+
+		service.saveFromCSV(csvFile, partyName);
+
 	}
 }
--- src/main/java/lt/itakademija/candidateCRUD/CandidateService.java
+++ src/main/java/lt/itakademija/candidateCRUD/CandidateService.java
@@ -1,10 +1,20 @@
 package lt.itakademija.candidateCRUD;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CandidateService {
@@ -13,8 +23,8 @@ public class CandidateService {
 	private CandidateRepository repository;
 
 	@Transactional
-	public CandidateEntity save(CandidateEntity c) {
-		return repository.save(c);
+	public void save(CandidateEntity c) {
+		repository.save(c);
 	}
 
 	@Transactional(readOnly = true)
@@ -25,6 +35,62 @@ public class CandidateService {
 	@Transactional
 	public CandidateEntity delete(Long id) {
 		return repository.delete(id);
+	}
+
+	@Transactional
+	public void saveFromCSV(MultipartFile CSVfile, String partyName) {
+
+		File file = new File(CSVfile.getOriginalFilename());
+		try {
+			file.createNewFile();
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(CSVfile.getBytes());
+			fos.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		String data = "";
+		String[] values = {};
+		String firsName = "";
+		String lastName = "";
+		String dateString = "";
+		String description = "";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+			while ((data = br.readLine()) != null) {
+
+				values = data.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+
+				firsName = values[0];
+				lastName = values[1];
+				dateString = values[2];
+				description = values[3];
+
+				repository.save(new CandidateEntity(firsName, lastName, parseDate(dateString), partyName, description));
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public Date parseDate(String dateString) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+
+		try {
+
+			date = dateFormat.parse(dateString);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return date;
 	}
 
 }
